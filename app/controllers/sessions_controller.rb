8203040201b6df0a22c37bb684edc408db1e549e
Chip Snyder <chip.snyder3@gmail.com>
2016-11-21 11:39:44 -0500

    End 8.1.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email:params[:session][:email].downcase)
+
+    if user && user.authenticate(params[:session][:passwoed])
+      #Log the user in
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+  
 end
