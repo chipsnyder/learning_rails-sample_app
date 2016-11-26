d412480e2120b7e2eda106f1ec3480f49a0245a2
Chip Snyder <chip.snyder3@gmail.com>
2016-11-26 15:31:32 -0500

    End 8.3

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,9 +1,5 @@
 module SessionsHelper
 
-  def log_in(user)
-    session[:user_id] = user.id
-  end
-
   def current_user
     @current_user ||= User.find_by(id: session[:user_id])
   end
@@ -11,4 +7,14 @@ module SessionsHelper
   def logged_in?
     !current_user.nil?
   end
+
+  def log_in(user)
+    session[:user_id] = user.id
+  end
+
+  def log_out
+    session.delete(:user_id)
+    @current_user = nil
+  end
+
 end
