0bbc5e5fdc4b9ce1d6747b6e7dfbda28b2fc51eb
Chip Snyder <chip.snyder3@gmail.com>
2016-11-28 23:32:36 -0500

    End 9.4

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -4,15 +4,36 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:lyra)
+    @nonadmin = users(:doge)
   end
 
   test "index including pagination" do
-    log_in_as(@user)
+    log_in_as(@nonadmin)
     get users_path
     assert_template 'users/index'
     assert_select 'div.pagination'
     User.paginate(page:1).each do |user|
       assert_select 'a[href=?]', user_path(user), text: user.name
+    end
+
+    assert_select 'a', text:'delete', count:0
+  end
+
+  test "index as admin including pagination and delete links" do
+    log_in_as(@user)
+    get users_path
+    assert_template 'users/index'
+    assert_select 'div.pagination'
+    first_page_of_users = User.paginate(page:1)
+    first_page_of_users.each do |user|
+      assert_select 'a[href=?]', user_path(user), text: user.name
+      unless user == @user
+        assert_select 'a[href=?]', user_path(user), text: 'delete', method: :delete
+      end
+    end
+
+    assert_difference 'User.count', -1 do
+      delete user_path(@nonadmin)
     end
   end
 end
