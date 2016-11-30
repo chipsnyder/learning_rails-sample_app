57be5a2e64f76c0ee71e1122da1c49aedf01cf36
Chip Snyder <chip.snyder3@gmail.com>
2016-11-29 23:08:56 -0500

    End 10.1.2

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,39 +2,40 @@ require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
 
-    test "invalid signup information" do
-      get signup_path
-      assert_no_difference 'User.count' do
-        post users_path, params: {
-          user:{
-            name:"",
-            email:"user@invalid",
-            password:"foo",
-            password_confirmation:"bar"
-          }
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: {
+        user:{
+          name:"",
+          email:"user@invalid",
+          password:"foo",
+          password_confirmation:"bar"
         }
-      end
-      assert_template 'users/new'
-      assert_select 'div#error_explanation'
-      assert_select 'div.field_with_errors'
+      }
     end
-
-    test "valid signup information" do
-      get signup_path
-      assert_difference 'User.count', 1 do
-        post users_path, params: {
-          user:{
-            name:"Lyra The Doge",
-            email:"eat@example.com",
-            password:"foodbar",
-            password_confirmation:"foodbar"
-          }
-        }
-        follow_redirect!
-      end
-      assert_template 'users/show'
-      assert_not flash.empty?
-      assert is_logged_in?
-    end
-
+    assert_template 'users/new'
+    assert_select 'div#error_explanation'
+    assert_select 'div.field_with_errors'
   end
+
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: {
+        user:{
+          name:"Lyra The Doge",
+          email:"eat@example.com",
+          password:"foodbar",
+          password_confirmation:"foodbar"
+        }
+      }
+      follow_redirect!
+    end
+    assert_not flash.empty?
+    assert_template 'static_pages/home'
+    # assert_template 'users/show'
+    # assert is_logged_in?
+  end
+
+end
