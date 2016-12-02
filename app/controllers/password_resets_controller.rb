d668d367e00478035993b95e9707889e27e1c755
Chip Snyder <chip.snyder3@gmail.com>
2016-12-01 23:36:48 -0500

    End 10.3

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -50,7 +50,8 @@ class PasswordResetsController < ApplicationController
   end
 
   def valid_user
-    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id])) redirect_to root_url
+    unless (@user && @user.activated? && @user.authenticated?(:reset, params[:id]))
+      redirect_to root_url
     end
   end
 
